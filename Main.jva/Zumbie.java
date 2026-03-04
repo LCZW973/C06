2c8788bff6149146dcd34ec33af42f1d2f486f5b
LCZW973 <leonardoczanin@gmail.com>
2026-03-04 10:41:06 -0300

    Update Zumbie.java

--- Main.jva/Zumbie.java
+++ Main.jva/Zumbie.java
@@ -1,9 +1,20 @@
 public class Zumbie {
     String nome;
     double vida;
+
     public void grito()
     {
         System.out.println( nome + " Ahhhh!!!!Ahhhh!!!! ");
+    }
+
+    public double mostraVida(){
+        System.out.print("o zumbie " + nome + " tem vida : ");
+        return vida;
+    }
+    public void transfereVida(Zumbie zumbiAlvo,double quantia)
+    {
+        zumbiAlvo.vida=zumbiAlvo.vida+quantia;
+        vida = vida-quantia;
     }
 
 }
