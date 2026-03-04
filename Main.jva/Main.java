f0babe28c550e51a94512683edb257a30332dfbc
LCZW973 <leonardoczanin@gmail.com>
2026-03-04 10:40:35 -0300

    Update Main.java

--- Main.jva/Main.java
+++ Main.jva/Main.java
@@ -6,14 +6,24 @@ public class Main {
         Zumbie z1 = new Zumbie();
         z1.nome="Chris";
         z1.vida=150;
-
         Zumbie z2 = new Zumbie();
         z2.nome="Leon";
         z2.vida=150;
+        double quantia;
 
         z1.grito();
+        System.out.println(z1.mostraVida());
         z2.grito();
+        System.out.println(z2.mostraVida());
+        System.out.println("");
 
-       sc.close();
+        System.out.println("Zumbie " + z1.nome + " comeca a tentar transferir vida para Zumbie " + z2.nome + " insira a quantidade de vida");
+        quantia = sc.nextDouble();
+        z1.transfereVida(z2,quantia);
+
+        System.out.println(z1.mostraVida());
+        System.out.println(z2.mostraVida());
+
+        sc.close();
     }
 }
