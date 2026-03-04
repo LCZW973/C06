f4392f1b395ed35adc67a75b3bcae1b3d23d4477
LCZW973 <leonardoczanin@gmail.com>
2026-03-04 10:14:26 -0300

    Update Main.java

--- Main.jva/Main.java
+++ Main.jva/Main.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        Zumbie z1 = new Zumbie();
+        z1.nome="Chris";
+        z1.vida=150;
+
+        Zumbie z2 = new Zumbie();
+        z2.nome="Leon";
+        z2.vida=150;
+
+        z1.grito();
+        z2.grito();
+
+       sc.close();
+    }
+}
