18fa078f4ef55799b0e4a4d6fa83e08eb31b6c24
LCZW973 <leonardoczanin@gmail.com>
2026-03-06 17:42:49 -0300

    Create Motor.java

---
+++ Aula4Ex6/Motor.java
@@ -0,0 +1,8 @@
+public class Motor {
+    String cilindradas;
+    float velocidadeMaxima;
+    public void mostraInfo(){
+        System.out.println(" Cilindradas : " + cilindradas);
+        System.out.println(" Velocidade Maxima : " + velocidadeMaxima);
+    }
+}
