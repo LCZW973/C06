5499b20ff552251a7f0b3fc481aebff4fa9ebeef
LCZW973 <leonardoczanin@gmail.com>
2026-03-10 10:28:28 -0300

    Update Main.java

--- Aula5Ex0/Main.java
+++ Aula5Ex0/Main.java
@@ -1,1 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+               Cantina cantina = new Cantina();
+               Salgado salgado1 = new Salgado();
+               Salgado salgado2 = new Salgado();
+               Salgado salgado3 = new Salgado();
+
+               salgado1.nome = "Coxinha";
+               salgado2.nome = "Pastel";
+               salgado3.nome = "Croquete";
+
+               cantina.addSalgado(salgado1);
+               cantina.addSalgado(salgado2);
+               cantina.addSalgado(salgado3);
+
+               cantina.mostraInfo();
+            }
+
+    }
 
