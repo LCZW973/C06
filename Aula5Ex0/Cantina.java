7c6d72d64dc488a2f707994cb09742247e8a6281
LCZW973 <leonardoczanin@gmail.com>
2026-03-10 10:29:52 -0300

    Create Cantina.java

---
+++ Aula5Ex0/Cantina.java
@@ -0,0 +1,21 @@
+public class Cantina {
+    String nome;
+    Salgado []salgado = new Salgado[5];
+    void addSalgado(Salgado novoSalgado){
+        for(int i = 0 ; i <salgado.length;i++)
+        {
+            if (salgado[i] == null) {
+                salgado[i] = novoSalgado;
+                break;
+            }
+        }
+    }
+    void mostraInfo(){
+        for(int i = 0 ; i < salgado.length;i++){
+            if(salgado[i] !=null)
+               System.out.println(salgado[i].nome);
+        }
+
+
+    }
+}
