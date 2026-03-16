89859aee93705c58b01aa2c250f5beaa7584292c
LCZW973 <leonardoczanin@gmail.com>
2026-03-16 14:31:15 -0300

    Update Main.java

--- Aula5Ex1/Main.java
+++ Aula5Ex1/Main.java
@@ -9,46 +9,51 @@ public class Main {
         campo.matriz = matriz;
 
         campo.criarcampo();
+        campo.inicializaganhador();
 
 
         System.out.println("Inserir o numero de bombas");
         int numerobombas =sc.nextInt();
         campo.addBomba(numerobombas);
+        int indicevitoria = 64 - numerobombas-1 ;
+
 
         int linha;
         int coluna;
         int escolha;
+        int i =  0 ;
+        int j = 0 ;
 
+        campo.matriz.mostrarmatriz();
 
         do {
-            campo.mostrarcampo();
-            System.out.println("Selecione 1 para inserir bandeira e 2 pra escolher casa ");
-            escolha = sc.nextInt();
-            if (escolha == 1)
-            {
+            do {
+                campo.mostrarcampo();
+                System.out.println("Selecione 1 para inserir bandeira e 2 pra escolher casa ");
+                escolha = sc.nextInt();
+                if (escolha == 1) {
+                    System.out.println("linha e coluna da  bandeira");
+                    linha = sc.nextInt();
+                    coluna = sc.nextInt();
+                    campo.inserirbandeira(linha, coluna);
+                }
+
+                System.out.println("Inserir linha e coluna deseejada");
                 linha = sc.nextInt();
                 coluna = sc.nextInt();
-                campo.inserirbandeira(linha,coluna);
+                j++;
+            } while (campo.comparacampo(linha, coluna));
+
+            if( campo.confereseganhou(linha,coluna,indicevitoria,i)) {
+                i++;
             }
 
-            System.out.println("Inserir linha e coluna deseejada");
-            linha = sc.nextInt();
-            coluna = sc.nextInt();
 
-        }while(campo.comparacampo(linha,coluna));
+        }while( campo.ganhador[indicevitoria] == 0 );
 
-
-
+        System.out.println(" Parabens ! voce chegou ao fim com " + j  + " jpgadas ");
         campo.mostrarcampo();
 
-
-
-
-
-
-
-
-
-
+       sc.close();
     }
 }
